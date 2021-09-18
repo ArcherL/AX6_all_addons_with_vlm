6059f7193ef173d231fc26f7cd30fc47a335d146
ArcherL <liuzihao2008@gmail.com>
2021-09-18 22:05:10 +0800

    pull source.

--- diy-part2.sh
+++ diy-part2.sh
@@ -31,6 +31,8 @@ git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git pac
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
 #添加smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
